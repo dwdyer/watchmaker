e69dcab936e2027b65eb1914b987f5307e5cbbfa
dwdyer <dan@uncommons.org>
2006-10-21 19:52:50 +0000

    Documentation tweaks.

--- framework/src/java/main/org/uncommons/watchmaker/framework/EvolutionPipeline.java
+++ framework/src/java/main/org/uncommons/watchmaker/framework/EvolutionPipeline.java
@@ -5,15 +5,19 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * A compound evolutionary operator that combines multiple operators of the
- * same type applied in series.
+ * A compound evolutionary operator that applies multiple operators (of the
+ * same type) in series.
+ *
+ * By combining EvolutionPipeline operators with {@link SplitEvolution} operators,
+ * elaborate evolutionary schemes can be constructed.
+ *
  * @author Daniel Dyer
  */
 public class EvolutionPipeline<T> implements EvolutionaryOperator<T>
 {
     private final List<EvolutionaryOperator<? super T>> pipeline;
 
-    
+
     public EvolutionPipeline(List<EvolutionaryOperator<? super T>> pipeline)
     {
         this.pipeline = new ArrayList<EvolutionaryOperator<? super T>>(pipeline);
