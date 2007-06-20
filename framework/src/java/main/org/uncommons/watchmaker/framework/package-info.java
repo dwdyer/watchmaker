87b465f1bc57d22c41ffe22f545aee42076fd772
dwdyer <dan@uncommons.org>
2007-06-20 22:09:34 +0000

    Include examples source code in release archives.

--- framework/src/java/main/org/uncommons/watchmaker/framework/package-info.java
+++ framework/src/java/main/org/uncommons/watchmaker/framework/package-info.java
@@ -14,7 +14,7 @@
 //   limitations under the License.
 // ============================================================================
 /**
- * This package provides a framework for evolutionary algorithms.  It defines generic
+ * This package provides a framework for evolutionary computation.  It defines generic
  * interfaces for evolutionary operators, fitness functions and selection strategies.
  * It also provides an all-purpose evolution engine.
  * @author Daniel Dyer
