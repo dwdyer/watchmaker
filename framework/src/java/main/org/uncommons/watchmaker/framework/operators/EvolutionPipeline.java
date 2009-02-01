a066ebefdcf85d49d8755c1bd1baefa97027e95b
dwdyer <dan@uncommons.org>
2009-02-01 13:46:29 +0000

    Updated copyright notices.

--- framework/src/java/main/org/uncommons/watchmaker/framework/operators/EvolutionPipeline.java
+++ framework/src/java/main/org/uncommons/watchmaker/framework/operators/EvolutionPipeline.java
@@ -1,5 +1,5 @@
 // ============================================================================
-//   Copyright 2006, 2007, 2008 Daniel W. Dyer
+//   Copyright 2006-2009 Daniel W. Dyer
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
