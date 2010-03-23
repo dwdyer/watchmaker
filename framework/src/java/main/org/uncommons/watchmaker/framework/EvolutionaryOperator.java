df5460352f9c59ed0de34252890af58e6d81f1aa
dwdyer <dan@uncommons.org>
2010-03-23 21:01:49 +0000

    Documentation improvements.

--- framework/src/java/main/org/uncommons/watchmaker/framework/EvolutionaryOperator.java
+++ framework/src/java/main/org/uncommons/watchmaker/framework/EvolutionaryOperator.java
@@ -19,9 +19,15 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * An evolutionary operator is a function that takes a population of
+ * <p>An evolutionary operator is a function that takes a population of
  * candidates as an argument and returns a new population that is the
- * result of applying a transformation to the original population.
+ * result of applying a transformation to the original population.</p>
+ * <p><strong>An implementation of this class must not modify any of
+ * the selected candidate objects passed in.</strong>  Doing so will
+ * affect the correct operation of the {@link EvolutionEngine}.  Instead
+ * the operator should create and return new candidate objects.  The
+ * operator is not required to create copies of unmodified individuals
+ * (for efficiency these may be returned directly).</p>
  * @param <T> The type of evolvable entity that this operator accepts.
  * @author Daniel Dyer
  */
@@ -44,7 +50,7 @@ public interface EvolutionaryOperator<T>
      * operators that process each candidate in isolation, such as mutation.
      * It should only be an issue for operators, such as cross-over, that
      * deal with multiple candidates in a single operation.</p>
-     * <p><strong>The operator should not modify any of the candidates passed
+     * <p><strong>The operator must not modify any of the candidates passed
      * in</strong>.  Instead it should return a list that contains evolved
      * copies of those candidates (umodified candidates can be included in
      * the results without having to be copied).</p>
